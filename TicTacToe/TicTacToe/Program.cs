19a5fb3a90d00a859dc1200bcd4362b31e0839fa
Alexander Shivinsky <shivinsky@gmail.com>
2011-09-03 17:33:39 +0300

    yaya!

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace TicTacToe
 {
-#if WINDOWS || XBOX
     static class Program
     {
         /// <summary>
@@ -16,6 +15,5 @@ namespace TicTacToe
             }
         }
     }
-#endif
 }
 
