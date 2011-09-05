72882f18cab9300b56b2feee5d0249276ed8d54d
Alexander Shivinsky <shivinsky@gmail.com>
2011-09-06 00:13:36 +0300

    clear

---
+++ TicTacToe/TicTacToe/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Player
+    {
+        Board _board;
+
+        public Player(Board board)
+        {
+            _board = board;
+        }
+
+        public int Score
+        {
+            get;
+            set;
+        }
+
+        public int Winner
+        {
+            get;
+            set;
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
